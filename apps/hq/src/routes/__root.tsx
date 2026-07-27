946cef3e679ecd3849aa1ae9e43b0ec09112aef0
Ryan Luong <lumintriet@gmail.com>
2026-07-27 20:00:24 +0700

    feat(hq/routes): add home page and generate route tree.

---
+++ apps/hq/src/routes/__root.tsx
@@ -0,0 +1,41 @@
+import type { ReactNode } from "react";
+
+import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
+
+import styles from "@/globals.css?url";
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        content: "width=device-width, initial-scale=1",
+        name: "viewport",
+      },
+      {
+        title: "HQ",
+      },
+    ],
+    links: [
+      { rel: "stylesheet", href: styles },
+      { type: "image/svg+xml", href: "/favicon.svg", rel: "icon" },
+    ],
+  }),
+  shellComponent: AppShell,
+});
+
+function AppShell({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <div className="h-screen">{children}</div>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
