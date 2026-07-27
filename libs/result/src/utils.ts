3334682065e01fa5a88a65d055dad49ac8904d9f
Ryan Luong <lumintriet@gmail.com>
2026-07-27 10:00:57 +0700

    feat(result/utils): define util functions.

---
+++ libs/result/src/utils.ts
@@ -0,0 +1,22 @@
+import type { AppError } from "@repo/error/types";
+
+import type { Err, Ok } from "@/types";
+
+export function ok<D>(data: D, metadata?: Record<string, unknown>): Ok<D> {
+  return {
+    ok: true,
+    data,
+    metadata,
+  };
+}
+
+export function err<E extends AppError<string>>(
+  error: E,
+  metadata?: Record<string, unknown>,
+): Err<E> {
+  return {
+    ok: false,
+    error,
+    metadata,
+  };
+}
