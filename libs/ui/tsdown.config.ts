a53a491b95defaf5a0e7f75c296c490fb2a03699
Ryan Luong <lumintriet@gmail.com>
2026-07-27 20:37:10 +0700

    build(ui): change style export.

--- libs/ui/tsdown.config.ts
+++ libs/ui/tsdown.config.ts
@@ -3,7 +3,7 @@ import { createReactLibConfig } from "@repo/tsdown-config";
 export default createReactLibConfig({
   exports: {
     customExports: {
-      "./styles.css": "./src/styles.css",
+      "./styles.css": "./src/styles/main.css",
     },
   },
 });
