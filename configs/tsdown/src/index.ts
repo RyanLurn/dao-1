86d6a0c6eb74d56283cf79ff49deabdc03a50c34
Ryan Luong <lumintriet@gmail.com>
2026-07-27 18:58:30 +0700

    feat(tsdown-config): make configs into functions.

--- configs/tsdown/src/index.ts
+++ configs/tsdown/src/index.ts
@@ -4,21 +4,32 @@ import babel from "@rolldown/plugin-babel";
 import { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "tsdown";
 
-export const libConfig = {
-  entry: ["src/**/*.{ts,tsx}", "!src/**/*.test.{ts,tsx}", "!src/try.ts"],
-  dts: {
-    sourcemap: true,
-  },
-  unbundle: true,
-  exports: true,
-} as const satisfies UserConfig;
+export function createLibConfig({
+  platform = "neutral",
+  exports = true,
+}: Pick<UserConfig, "platform" | "exports">): UserConfig {
+  return {
+    entry: ["src/**/*.{ts,tsx}", "!src/**/*.test.{ts,tsx}", "!src/try.ts"],
+    dts: {
+      sourcemap: true,
+    },
+    unbundle: true,
+    platform,
+    exports,
+  };
+}
 
-export const reactLibConfig = defineConfig({
-  ...libConfig,
-  platform: "neutral",
-  plugins: [
-    babel({
-      presets: [reactCompilerPreset()],
-    }),
-  ],
-});
+export function createReactLibConfig({
+  platform = "neutral",
+  exports = true,
+}: Pick<UserConfig, "platform" | "exports">) {
+  const libConfig = createLibConfig({ platform, exports });
+  return defineConfig({
+    ...libConfig,
+    plugins: [
+      babel({
+        presets: [reactCompilerPreset()],
+      }),
+    ],
+  });
+}
--- configs/tsdown/tsdown.config.ts
+++ configs/tsdown/tsdown.config.ts
@@ -1,3 +1,3 @@
-import { libConfig } from "./src/index.ts";
+import { createLibConfig } from "./src/index.ts";
 
-export default libConfig;
+export default createLibConfig({ platform: "node" });
