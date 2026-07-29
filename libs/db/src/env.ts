1bdf9763fc5538cb48dd43bfe76d4464692fa4fc
Ryan Luong <lumintriet@gmail.com>
2026-07-29 08:07:32 +0700

    feat(db/env): declare DbEnvSchema.

---
+++ libs/db/src/env.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const DbEnvSchema = z.object({
+  NEON_POOLED_CONNECTION_STRING: z.templateLiteral([
+    "postgresql://",
+    z.string().min(1),
+    ":",
+    z.string().min(1),
+    "@",
+    z.string().min(1),
+    "-pooler.",
+    z.string().min(1),
+    ".neon.tech/",
+    z.string().min(1),
+  ]),
+});
