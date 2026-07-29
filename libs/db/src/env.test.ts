9845f4d9c939f750fe949c3635897a7f5133a290
Ryan Luong <lumintriet@gmail.com>
2026-07-29 09:01:39 +0700

    test(db/env): test the failure case of DbEnvSchema.

--- libs/db/src/env.test.ts
+++ libs/db/src/env.test.ts
@@ -1,6 +1,11 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 
 import { DbEnvSchema } from "@/env";
+
+const validKey = "NEON_POOLED_CONNECTION_STRING";
+
+const invalidValue =
+  "postgresql://user1:AbC123dEf@ep-cool-darkness-123456.us-east-2.aws.neon.tech/dbname?sslmode=require";
 
 // Success case
 test("DbEnvSchema should validate a valid Neon pooled connection string", () => {
@@ -13,4 +18,46 @@ test("DbEnvSchema should validate a valid Neon pooled connection string", () => 
 
   expect.assert(parseResult.success === true);
   expect(parseResult.data).toBe(pooledConnectionString);
+});
+
+// Failure cases
+describe("DbEnvSchema should fail to validate", () => {
+  test("an object without the key `NEON_POOLED_CONNECTION_STRING`", () => {
+    const envObject = {
+      NODE_ENV: "production",
+    };
+
+    const parseResult = DbEnvSchema.safeParse(envObject);
+    expect.assert(parseResult.success === false);
+
+    const issues = parseResult.error.issues;
+    expect(issues).toHaveLength(1);
+
+    const issue = issues[0];
+    expect(issue).toMatchObject({
+      code: "invalid_type",
+      expected: "string",
+      path: [validKey],
+    });
+  });
+
+  test("an object with the right key but an invalid value", () => {
+    const envObject = {
+      NODE_ENV: "production",
+      [validKey]: invalidValue,
+    };
+
+    const parseResult = DbEnvSchema.safeParse(envObject);
+    expect.assert(parseResult.success === false);
+
+    const issues = parseResult.error.issues;
+    expect(issues).toHaveLength(1);
+
+    const issue = issues[0];
+    expect(issue).toMatchObject({
+      code: "invalid_format",
+      format: "template_literal",
+      path: [validKey],
+    });
+  });
 });
