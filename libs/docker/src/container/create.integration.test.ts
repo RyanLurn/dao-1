257c1a3fcb69ba0dc46a278d080a679cc141d087
Ryan Luong <lumintriet@gmail.com>
2026-07-30 10:38:52 +0700

    test(docker/container): write the 404 failure case for createDockerContainer function integration test.

--- libs/docker/src/container/create.integration.test.ts
+++ libs/docker/src/container/create.integration.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, expect, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 
 import { DockerClient } from "@/client";
 import { createDockerContainer } from "@/container/create";
@@ -24,4 +24,23 @@ test("createDockerContainer function should create a Docker container", async ()
   expect(statusCode).toBe(201);
   expect(parsedBody).toHaveProperty("Id");
   expect(parsedBody).toHaveProperty("Warnings");
+});
+
+// Failure cases
+describe("createDockerContainer function should return", async () => {
+  test("a 404 status code for an image that doesn't exist", async () => {
+    const nonExistentImageName = "no-such-image:latest";
+
+    const { statusCode, parsedBody } = await createDockerContainer({
+      dockerClient,
+      containerName: "createDockerContainer-integration-test-404-case",
+      imageName: nonExistentImageName,
+    });
+
+    expect(statusCode).toBe(404);
+    expect(parsedBody).toHaveProperty(
+      "message",
+      `No such image: ${nonExistentImageName}`,
+    );
+  });
 });
