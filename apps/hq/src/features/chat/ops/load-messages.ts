df8fc3f59d6fd457b51efc1d47c22c80a1d326ac
Ryan Luong <lumintriet@gmail.com>
2026-08-01 20:30:24 +0700

    feat(hq/chat): define loadMessages function.

--- apps/hq/src/features/chat/ops/load-messages.ts
+++ apps/hq/src/features/chat/ops/load-messages.ts
@@ -1,18 +1,32 @@
-import { dbConnect } from "@repo/db/connect";
+import type { FlatError } from "@repo/error/types";
+import type { Result } from "@repo/result/types";
+
+import { db } from "@repo/db";
 import { messageTable } from "@repo/db/schema/tables/message";
+import { UnexpectedError } from "@repo/error/classes/unexpected";
+import { err, ok } from "@repo/result/utils";
 import { createServerFn } from "@tanstack/react-start";
 import { setResponseStatus } from "@tanstack/react-start/server";
 
 export const loadMessages = createServerFn({ strict: false }).handler(
-  async () => {
-    const connectResult = dbConnect();
-    if (!connectResult.ok) {
+  async (): Promise<
+    Result<
+      Array<typeof messageTable.$inferSelect>,
+      FlatError<UnexpectedError["code"]>
+    >
+  > => {
+    try {
+      const messages = await db.select().from(messageTable);
+      return ok(messages);
+    } catch (error) {
+      console.error(error);
       setResponseStatus(500, "Internal Server Error");
-      return connectResult.error.serialize({ mode: "flat" });
+      return err(
+        new UnexpectedError({
+          action: "load messages",
+          cause: error,
+        }).serialize({ mode: "flat" }),
+      );
     }
-    const db = connectResult.data;
-
-    const messages = await db.select().from(messageTable);
-    return messages;
   },
 );
