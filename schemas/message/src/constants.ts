c3193d4c3a181f56a0263dccf50193128e6afce7
Ryan Luong <lumintriet@gmail.com>
2026-08-01 18:59:53 +0700

    feat(message-schemas/constants): add more constants.

--- schemas/message/src/constants.ts
+++ schemas/message/src/constants.ts
@@ -1,3 +1,19 @@
+export const TEXT_PART_TYPE = "text";
+export const TEXT_PART_STREAMING_STATE = "streaming";
+export const TEXT_PART_DONE_STATE = "done";
+export const TEXT_PART_STATE_LIST = [
+  TEXT_PART_STREAMING_STATE,
+  TEXT_PART_DONE_STATE,
+] as const;
+
+export const REASONING_PART_TYPE = "text";
+export const REASONING_PART_STREAMING_STATE = "streaming";
+export const REASONING_PART_DONE_STATE = "done";
+export const REASONING_PART_STATE_LIST = [
+  REASONING_PART_STREAMING_STATE,
+  REASONING_PART_DONE_STATE,
+] as const;
+
 export const MESSAGE_SYSTEM_ROLE = "system";
 export const MESSAGE_USER_ROLE = "user";
 export const MESSAGE_ASSISTANT_ROLE = "assistant";
