ce8104ec4adb7a0f031745e3f2f2448621023930
Ryan Luong <lumintriet@gmail.com>
2026-08-01 09:14:04 +0700

    wip(api/services/agent): throw HTTPError on invalid agent id.

---
+++ apps/api/src/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_RESPONSE_HEADERS = {
+  "Content-Type": "application/json",
+} as const;
--- apps/api/src/services/agent.ts
+++ apps/api/src/services/agent.ts
@@ -1,12 +1,29 @@
+import type { InvalidAgentIdErrorResponseBody } from "@repo/agent-schemas";
+
 import { AgentIdSchema } from "@repo/agent-schemas";
-import { H3 } from "h3";
+import { AGENT_SERVICE_ERROR_RECORD } from "@repo/agent-schemas/constants";
+import { H3, HTTPError } from "h3";
+
+import { DEFAULT_RESPONSE_HEADERS } from "@/constants";
 
 export const agentService = new H3().patch(
   "/:agentId/mode",
   async ({ context }) => {
     const parseIdResult = AgentIdSchema.safeParse(context.params?.agentId);
     if (!parseIdResult.success) {
-      return {};
+      throw new HTTPError({
+        status: AGENT_SERVICE_ERROR_RECORD.INVALID_AGENT_ID_ERROR.status.code,
+        statusText:
+          AGENT_SERVICE_ERROR_RECORD.INVALID_AGENT_ID_ERROR.status.text,
+        headers: DEFAULT_RESPONSE_HEADERS,
+        body: {
+          ok: false,
+          error: {
+            code: "INVALID_AGENT_ID_ERROR",
+            cause: "Invalid UUID",
+          },
+        } satisfies InvalidAgentIdErrorResponseBody,
+      });
     }
   },
 );
