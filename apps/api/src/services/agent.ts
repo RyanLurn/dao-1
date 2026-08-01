1b514a7576c99f193590988443dcd8ae223e4310
Ryan Luong <lumintriet@gmail.com>
2026-08-01 08:43:46 +0700

    wip(api/services/agent)

--- apps/api/src/services/agent.ts
+++ apps/api/src/services/agent.ts
@@ -1,6 +1,12 @@
+import { AgentIdSchema } from "@repo/agent-schemas";
 import { H3 } from "h3";
 
 export const agentService = new H3().patch(
   "/:agentId/mode",
-  async ({ context }) => {},
+  async ({ context }) => {
+    const parseIdResult = AgentIdSchema.safeParse(context.params?.agentId);
+    if (!parseIdResult.success) {
+      return {};
+    }
+  },
 );
