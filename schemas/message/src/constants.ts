cebd1353590d57464af57cd6c73e56714a5dce56
Ryan Luong <lumintriet@gmail.com>
2026-08-01 18:54:47 +0700

    feat(message-schemas/constants): define MESSAGE_ROLE_LIST.

---
+++ schemas/message/src/constants.ts
@@ -0,0 +1,8 @@
+export const MESSAGE_SYSTEM_ROLE = "system";
+export const MESSAGE_USER_ROLE = "user";
+export const MESSAGE_ASSISTANT_ROLE = "assistant";
+export const MESSAGE_ROLE_LIST = [
+  MESSAGE_SYSTEM_ROLE,
+  MESSAGE_USER_ROLE,
+  MESSAGE_ASSISTANT_ROLE,
+] as const;
