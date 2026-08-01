01dbc62a559477febe68184c84ebcd100e23e4ee
Ryan Luong <lumintriet@gmail.com>
2026-08-01 19:25:51 +0700

    feat(db): remove unused code and run build.

--- libs/db/src/schema/helpers/user-id.ts
+++
@@ -1,7 +0,0 @@
-import { uuid } from "drizzle-orm/pg-core";
-
-import { userTable } from "@/schema/tables/user";
-
-export const userId = uuid("user_id")
-  .notNull()
-  .references(() => userTable.id, { onDelete: "cascade" });
