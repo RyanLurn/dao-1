e6afa7e91dff16009eca7f0393ec160bdc9a6d4b
Ryan Luong <lumintriet@gmail.com>
2026-08-01 07:35:04 +0700

    feat(api/config): define EnvSchema.

---
+++ apps/api/src/config/env.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const EnvSchema = z.object({
+  NODE_ENV: z.enum(["development", "testing", "staging", "production"]),
+  DB_FILE_PATH: z.templateLiteral([
+    z.string().min(1),
+    ".",
+    z.enum(["db", "sqlite"]),
+  ]),
+});
